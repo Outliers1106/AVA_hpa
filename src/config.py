017d50a9b64b4db6069a5470dc5816441ec4211b
Outliers1106 <tuyanlun9716@163.com>
2021-01-15 18:28:10 +0800

    2021 0115 no pretrain 10 class 613 split

--- src/config.py
+++ src/config.py
@@ -54,7 +54,7 @@ def get_pretrain_config():
 
 def get_train_config():
     time_prefix = time.strftime("-%Y%m%d-%H%M%S", time.localtime())
-    prefix = "AVA-hpa-train-resnet18-10-217"
+    prefix = "AVA-hpa-train-resnet18-10-613"
     config = ed({
         # base setting
         "description": "this is the description for currnet config file.",
