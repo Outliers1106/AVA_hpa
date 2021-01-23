eef0576116de8eae2891abdcbb5792f883358f23
Outliers1106 <tuyanlun9716@163.com>
2021-01-23 23:56:05 +0800

    217 27 aag pretrain

--- scripts/run_eval.sh
+++ scripts/run_eval.sh
@@ -9,11 +9,11 @@ echo "==========================================================================
 PROJECT_DIR=$(cd "$(dirname "$0")" || exit; pwd)
 
 python ${PROJECT_DIR}/../eval.py \
-    --device_id 4 \
+    --device_id 3 \
     --device_num 1 \
     --device_target Ascend \
     --model_arch resnet18 \
     --classes 27 \
-    --ckpt_path /home/tuyanlun/code/mindspore_r1.0/hpa/AVA-hpa-train-resnet18-27-217/checkpoint-20210115-133112/AVA-20_4590.ckpt\
+    --ckpt_path /home/tuyanlun/code/mindspore_r1.0/hpa/AVA-hpa-train-resnet18-27-217/checkpoint-20210115-113850/AVA-20_4590.ckpt\
     --data_dir /home/tuyanlun/code/mindspore_r1.0/hpa_dataset/hpa \
-    --save_eval_path ./217_27_no_pretrain/
+    --save_eval_path ./217_27_aag_pretrain/
