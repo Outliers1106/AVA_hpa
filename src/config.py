06ea0837e8e72286a20a7411b3505ea811f6869d
Outliers1106 <tuyanlun9716@163.com>
2021-02-03 14:22:17 +0800

    update readme

--- src/config.py
+++ src/config.py
@@ -10,7 +10,7 @@ import os
 
 def get_pretrain_config():
     time_prefix = time.strftime("-%Y%m%d-%H%M%S", time.localtime())
-    prefix = "AVA-hpa-pretrain-resnet18-27-613"
+    prefix = "AVA-hpa-pretrain"
     config = ed({
         # base setting
         "description": "this is the description for currnet config file.",
@@ -28,7 +28,7 @@ def get_pretrain_config():
         # dataset
         "dataset": "hpa",
         "bag_size": 1,
-        "classes": 27,
+        "classes": 10,
         "num_parallel_workers": 8,
 
         # optimizer
@@ -51,7 +51,7 @@ def get_pretrain_config():
 
 def get_train_config():
     time_prefix = time.strftime("-%Y%m%d-%H%M%S", time.localtime())
-    prefix = "AVA-hpa-train-resnet18-27-613"
+    prefix = "AVA-hpa-train"
     config = ed({
         # base setting
         "description": "this is the description for currnet config file.",
@@ -70,7 +70,7 @@ def get_train_config():
         "dataset": "hpa",
         "bag_size_for_train": 1,
         "bag_size_for_eval": 20,
-        "classes": 27,
+        "classes": 10,
         "num_parallel_workers": 8,
 
         # optimizer
