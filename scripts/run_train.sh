73697a0f8b38d318873257e214388f870bac5cba
Outliers1106 <tuyanlun9716@163.com>
2021-03-25 11:45:24 +0800

    20210325:only train fc on nt pretrain model

--- scripts/run_train.sh
+++ scripts/run_train.sh
@@ -6,11 +6,11 @@ echo "==========================================================================
 PROJECT_DIR=$(cd "$(dirname "$0")" || exit; pwd)
 
 python ${PROJECT_DIR}/../train.py \
-    --device_id 1 \
+    --device_id 2 \
     --device_num 1 \
     --device_target Ascend \
     --data_dir /home/tuyanlun/code/mindspore_r1.0/hpa_dataset/hpa \
     --save_checkpoint_path /home/tuyanlun/code/mindspore_r1.0/hpa/hpa_new_exp \
     --log_path /home/tuyanlun/code/mindspore_r1.0/hpa/hpa_new_exp \
-    --load_ckpt_path /home/tuyanlun/code/mindspore_r1.0/hpa/hpa_new_exp/AVA-hpa-pretrain-new-exp/checkpoint-20210324-184521/AVA-40_2185.ckpt \
+    --load_ckpt_path /home/tuyanlun/code/mindspore_r1.0/hpa/hpa_new_exp/AVA-hpa-pretrain-new-exp/checkpoint-20210324-184628/AVA-40_2185.ckpt \
     --save_eval_path /home/tuyanlun/code/mindspore_r1.0/hpa/hpa_new_exp
