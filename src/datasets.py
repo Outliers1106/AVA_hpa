8bbb42c0cf30241d532636c8eb610299f9fd0ac4
Outliers1106 <tuyanlun9716@163.com>
2021-04-03 22:55:56 +0800

    20210403--color-color-pretrain

--- src/datasets.py
+++ src/datasets.py
@@ -25,11 +25,13 @@ class TransformOnImg:
         rand_augment = RandAugment(n=2, m=10)
         self.trsfm_basic = Compose([
             transforms.ToPIL(),
-            transforms.Resize(256),
+            #transforms.Resize(256),
+            transforms.Resize(224),
             #transforms.RandomResizedCrop(size=224, scale=(0.2, 1.)),
             #transforms.RandomResizedCrop(size=224, scale=(0.2, 1.)),
             transforms.RandomColorAdjust(0.4, 0.4, 0.4, 0.4),
-            transforms.RandomResizedCrop(size=224, scale=(0.2, 1.)),
+            transforms.RandomColorAdjust(0.4, 0.4, 0.4, 0.4),
+            #transforms.RandomResizedCrop(size=224, scale=(0.2, 1.)),
             #transforms.RandomGrayscale(prob=0.2),
             transforms.RandomHorizontalFlip(), # set default
             transforms.ToTensor(),
