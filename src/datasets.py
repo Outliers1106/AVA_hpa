3f77f23ea34f662fe2e62d3cdf4a32c4b5bee7b9
Outliers1106 <tuyanlun9716@163.com>
2021-04-03 22:48:23 +0800

    20210403--color-crop-pretrain

--- src/datasets.py
+++ src/datasets.py
@@ -26,10 +26,11 @@ class TransformOnImg:
         self.trsfm_basic = Compose([
             transforms.ToPIL(),
             transforms.Resize(256),
-            transforms.RandomResizedCrop(size=224, scale=(0.2, 1.)),
             #transforms.RandomResizedCrop(size=224, scale=(0.2, 1.)),
-            #transforms.RandomColorAdjust(0.4, 0.4, 0.4, 0),
-            transforms.RandomGrayscale(prob=0.2),
+            #transforms.RandomResizedCrop(size=224, scale=(0.2, 1.)),
+            transforms.RandomColorAdjust(0.4, 0.4, 0.4, 0.4),
+            transforms.RandomResizedCrop(size=224, scale=(0.2, 1.)),
+            #transforms.RandomGrayscale(prob=0.2),
             transforms.RandomHorizontalFlip(), # set default
             transforms.ToTensor(),
             transforms.Normalize((0.4914, 0.4822, 0.4465), (0.2023, 0.1994, 0.2010)),
